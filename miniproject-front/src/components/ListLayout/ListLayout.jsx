58b171ef4d90c3e297cfddcf47df35558db62612
gyeolhwi <gyeori0626@gmail.com>
2024-08-07 02:23:23 +0900

    리스트페이지 탱구 들어갑니다

--- miniproject-front/src/components/ListLayout/ListLayout.jsx
+++ miniproject-front/src/components/ListLayout/ListLayout.jsx
@@ -1,11 +1,316 @@
 import React from 'react';
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
+import dog from '../../assets/강아지.jpg'
 
 function ListLayout(props) {
     return (
         <div css={s.layout}>
-            
+            <div css={s.container}>
+                <h2>리스트페이지</h2>
+                <div css={s.dataContainer}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>카테고리1</th>
+                                <th>카테고리2</th>
+                                <th>카테고리3</th>
+                                <th>카테고리4</th>
+                                <th>카테고리5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지1"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지2"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지3"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지4"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지5"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                                <td>
+                                    <p>소제목</p>
+                                    <img src={dog} alt="이미지"/>
+                                </td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }

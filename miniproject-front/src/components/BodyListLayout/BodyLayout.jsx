3f4093d4043ae7f2618ff5110249dd3292f622c4
gyeolhwi <gyeori0626@gmail.com>
2024-08-12 00:20:54 +0900

    0811 뉴스 날짜,순위추가

---
+++ miniproject-front/src/components/BodyListLayout/BodyLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import * as s from './style';
+import MainListLayout from '../List/MainListLayout/MainListLayout';
+import SubListLayout from '../List/SubListLayout/SubListLayout';
+
+function BodyLayout() {
+    return (
+        <div css={s.layout}>
+            <div css={s.container}>
+                <MainListLayout/>
+                <SubListLayout/>
+            </div>
+        </div>
+    );
+}
+
+export default BodyLayout;
---
+++ miniproject-front/src/constants/LISTSAMPLE.js
@@ -0,0 +1,12 @@
+export const SAMPLE_LIST = [
+    { text: "Lorem ipsum dolor sit amet" },
+    { text: " consectetur adipisicing elit, " },
+    { text: "sed do eiusmod tempor incididunt " },
+    { text: "ut labore et dolore magna aliqua. Ut enim" },
+    { text: "ad minim veniam, quis nostrud exercitation ullam" },
+    { text: "uis aute irure dolor in reprehenderit in volupta" },
+    { text: "te velit esse cillum dolore eu fugiat nulla pariatLorem ipsum dolor sit amet" },
+    { text: "ur. Excepteur sint occaecat cupidatat non proident, su" },
+    { text: "nt in culpa qui officia deserunt mollit anim id est laborum." },
+    { text: "Lorem ipsum dolor sit amet" },
+];

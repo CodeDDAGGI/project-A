b99bcad49f360529d3b5247a59af60279b1916eb
ehddls98 <icbku1357@naver.com>
2024-09-07 16:03:21 +0900

    back

--- miniproject-back/src/main/java/com/projectA/miniproject/controller/UserController.java
+++ miniproject-back/src/main/java/com/projectA/miniproject/controller/UserController.java
@@ -1,11 +1,6 @@
 package com.projectA.miniproject.controller;
 
-<<<<<<< HEAD
-import com.projectA.miniproject.dto.Request.ReqJoinUserDto;
-import com.projectA.miniproject.dto.Request.ReqSigninDto;
-=======
 import com.projectA.miniproject.dto.Request.ReqSignupDto;
->>>>>>> b5708a9ad2a9c292cc207289c89175364c950753
 import com.projectA.miniproject.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,58 +15,12 @@ import javax.validation.Valid;
 public class UserController {
 
     @Autowired
-    private UserService  userService;
+    private UserService userService;
 
     //post 추가
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid   @RequestBody ReqSignupDto joinUser){
+    public ResponseEntity<?> signup(@Valid @RequestBody ReqSignupDto joinUser) {
         log.info("{}", joinUser);
         return ResponseEntity.ok().body(userService.SignupUser(joinUser));
     }
-
-    @PostMapping("/signin")
-    public ResponseEntity<?> signin(@RequestBody ReqSigninDto dto) {
-        System.out.println(dto);
-        return ResponseEntity.ok().body(null);
-
-    }
-
-
-
-
-
-
-
 }
-
-
-
-//
-//
-//
-//
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> getId(@PathVariable int id){
-//        try {
-//            log.info("{}", id);
-//            return ResponseEntity.ok().body(id);
-//        } catch (Exception e) {
-//            log.error("Error occurred", e);
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred");
-//        }
-//    }
-

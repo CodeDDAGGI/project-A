63fc44f0a58ddb4297788c2ba40b56bcc0a8b03a
gyeolhwi <gyeori0626@gmail.com>
2024-09-07 18:16:56 +0900

    업로드에 등록하기전까지 완성

--- miniproject-front/src/pages/News/WritePage/WritePage.jsx
+++ miniproject-front/src/pages/News/WritePage/WritePage.jsx
@@ -1,22 +1,30 @@
-import React, { useMemo, useRef } from 'react';
-import Header from '../../../components/Header/Header';
-import 'react-quill/dist/quill.snow.css';
-import ReactQuill, { Quill } from 'react-quill';
-// import ImageResize from "quill-image-resize";
-import { css } from '@emotion/react';
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React, { useMemo, useRef, useState } from 'react';
+import Header from '../../../components/Header/Header';
+import ReactQuill, { Quill } from 'react-quill';
+import ImageResize from "quill-image-resize";
+import 'react-quill/dist/quill.snow.css';
 
-// Quill.register("modules/imageResize", ImageResize);
+Quill.register('modules/imageResize', ImageResize);
 
+const layout = css`
+    display: flex;
+    flex-direction: column;
+    padding: 0 0 20px 0;
+    height: 100%;
+`;
 const editorLayout = css`
     margin: 15px 0 0 0;
     box-sizing: border-box;
     margin-bottom: 42px;
+    min-height: 400px;
+    flex-grow: 1;
     width: 100%;
-    height: 700px;  
 `;
 const editorTitle = css`
     box-sizing: border-box;
+    margin: 0 0 15px 0;
     & > input {
         box-sizing: border-box;
         width: 100%;
@@ -26,16 +34,68 @@ const editorTitle = css`
     }
 
 `;
+const mainImgUpLoadBox = css`
+    display: flex;
+    width: 100%;
+    & > div> button {
+        width: fit-content;
+        margin: 0 10px 0 0;
+    }
+    & > :nth-last-of-type(1){
+        /* width: 100%; */
+        display: flex;
+        justify-content: flex-end;
+        flex-grow: 1;
+    }
+`;
+const buttonBox = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+`;
 function WritePage(props) {
+
     const quillRef = useRef(null);
+    const [board, setBoard] = useState({
+        title: "",
+        content: "",
+        img: "",
+    });
+    const [fileName, setFileName] = useState("");
+    console.log(fileName);
+
     const handleImgOnClick = () => {
         const fileInput = document.createElement("input");
         fileInput.setAttribute("type", "file");
         fileInput.setAttribute("accept", "image/*");
         fileInput.click();
+        fileInput.onchange = (e) => {
+            const uploadFile = e.target.files[0];
+            console.log(uploadFile);
+            setBoard(board => ({
+                ...board,
+                img: uploadFile.name,
+            }))
+        }
+    }
+    const handleInputOnChange = (e) => {
+        setBoard(board => ({
+            ...board,
+            [e.target.name]: e.target.value,
+        }))
+        console.log(board);
+    }
+
+    const handleQuillValueOnChange = (value) => {
+        setBoard(board => ({
+            ...board,
+            content: quillRef.current.getEditor().getText().trim() === "" ? "" : value,
+        }));
+    }
+    const handleSubmitOnClick = () => {
+        console.log(board);
         
-
-
     }
     const toolbarOptions = useMemo(() => [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -49,19 +109,25 @@ function WritePage(props) {
     ], []);
 
     return (
-        <>
+        <div>
             <Header />
-            <div>
+            <div css={layout}>
                 <div>
                     <h2>작성페이지</h2>
                 </div>
-                <div>
-                    <button onClick={handleImgOnClick}>메인사진업로드</button>
+                <div css={mainImgUpLoadBox}>
+                    <div>
+                        <button onClick={handleImgOnClick}>메인사진업로드</button>
+                        <span>{fileName}</span>
+                    </div>
+                    <div>
+                        <button onClick={handleSubmitOnClick}>등록하기</button>
+                    </div>
                 </div>
 
                 <div css={editorLayout}>
                     <div css={editorTitle}>
-                        <input type="text" placeholder='제목을 입력하세요' />
+                        <input type="text" name='title' onChange={handleInputOnChange} value={board.title} placeholder='제목을 입력하세요' />
                     </div>
                     <ReactQuill
                         ref={quillRef}
@@ -70,18 +136,23 @@ function WritePage(props) {
                             width: "100%",
                             height: "100%"
                         }}
-                        modules={
-                            {
-                                toolbar: {
-                                    container: toolbarOptions,
-                                },
+                        onChange={handleQuillValueOnChange}
+                        modules={{
+                            toolbar: {
+                                container: toolbarOptions,
+                                // handlers: {
+                                //     // image: handleImageLoad,
+                                // }
+                            },
+                            imageResize: {
+                                parchment: Quill.import("parchment")
+                            },
 
-                            }
-                        }
+                        }}
                     />
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
